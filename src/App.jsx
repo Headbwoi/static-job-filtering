f7247d6524dfa838700f59d60163f0da7369d4ad
headbwoi <tochukwujohn24@gmail.com>
2022-08-06 16:01:08 +0100

    ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import Cards from "./components/Cards"
 import Header from "./components/Header"
 
 function App() {
   return (
     <>
       <Header />
+      <Cards />
     </>
   )
 }
